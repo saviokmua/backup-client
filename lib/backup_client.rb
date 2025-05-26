31915c8d2a63ca88886d9253837684f1ec55dc33
Alex <savio.km.ua@gmail.com>
2025-05-26 19:59:49 +0300

    Fix

--- lib/backup_client.rb
+++ lib/backup_client.rb
@@ -17,7 +17,7 @@ Dir.glob("components/**/*.rb").sort.each do |file|
 end
 
 module BackupClient
-  VERSION = "0.1.7"
+  VERSION = "0.1.8"
 
   class Processor
     include ::BackupClient::Helpers::LogHelper
